0720d7c706c7dac88141f9032a64b28f318a9a61
Zidane Ali <basalamah76@gmail.com>
2022-03-22 16:17:43 +0700

    make it better

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../Models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 
