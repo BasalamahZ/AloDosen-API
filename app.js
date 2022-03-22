67f965096374a11e934900b63fe51fb251cda241
Zidane Ali <basalamah76@gmail.com>
2022-03-22 15:57:31 +0700

    make it better

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.use("/api/conversation", conversationRoute);
 app.use("/api/message", messageRoute);
 
 // Start Server
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server Running at http://localhost:${port}`);
 });
